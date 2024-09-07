702a4db604dfd98f5dae4b6524955e91a3d71924
underthehoody <xxxlhoodie@gmail.com>
2024-09-07 13:56:06 -0400

    first draft to be published.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,12 @@
+const buttonAbh = document.getElementById("abh-btn");
+const buttonBanterok = document.getElementById("banterok-btn");
 const buttonDragon = document.getElementById("dragon-btn");
 const buttonDragonborn = document.getElementById("dragonborn-btn");
-const buttonAbh = document.getElementById("abh-btn");
+const buttonHeartbeat = document.getElementById("heartbeat-btn");
+const buttonOpera = document.getElementById("opera-btn");
+const buttonParadigm = document.getElementById("paradigm-btn");
 const buttonSilo = document.getElementById("silo-btn");
+const buttonColdRow = document.getElementById("coldrow-btn");
 
 const lore = document.getElementById("lore");
 
@@ -30,4 +35,24 @@ buttonAbh.addEventListener(
 buttonSilo.addEventListener(
 	"click",
 	() => showLore("lorepages/silos.html"),
+);
+buttonHeartbeat.addEventListener(
+	"click",
+	() => showLore("lorepages/heartbeat.html"),
+);
+buttonColdRow.addEventListener(
+	"click",
+	() => showLore("lorepages/coldrow.html"),
+);
+buttonOpera.addEventListener(
+	"click",
+	() => showLore("lorepages/opera.html"),
+);
+buttonBanterok.addEventListener(
+	"click",
+	() => showLore("lorepages/banterok.html"),
+);
+buttonParadigm.addEventListener(
+	"click",
+	() => showLore("lorepages/paradigm.html"),
 );
